4ee33145bcff893adc7a54a02efc3580849e40be
Skylabzz <punnawich.cs@gmail.com>
2025-04-11 10:24:29 +0700

    Add files via upload

---
+++ ProjectFallDetectToLineNotify/predict_camera.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2025 at 05:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `predict_camera`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `camera`
+--
+
+CREATE TABLE `camera` (
+  `id` int(11) NOT NULL,
+  `camera_name` varchar(100) DEFAULT NULL,
+  `stream_url` varchar(255) DEFAULT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `message_id` int(11) DEFAULT NULL,
+  `line_token_id` int(11) DEFAULT NULL,
+  `is_streaming` tinyint(1) DEFAULT NULL,
+  `is_notification` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `line_token`
+--
+
+CREATE TABLE `line_token` (
+  `id` int(11) NOT NULL,
+  `token` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message_template`
+--
+
+CREATE TABLE `message_template` (
+  `id` int(11) NOT NULL,
+  `content` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `room_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `camera`
+--
+ALTER TABLE `camera`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `stream_url` (`stream_url`),
+  ADD KEY `room_id` (`room_id`),
+  ADD KEY `message_id` (`message_id`),
+  ADD KEY `line_token_id` (`line_token_id`),
+  ADD KEY `ix_camera_id` (`id`),
+  ADD KEY `ix_camera_camera_name` (`camera_name`);
+
+--
+-- Indexes for table `line_token`
+--
+ALTER TABLE `line_token`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `ix_line_token_id` (`id`);
+
+--
+-- Indexes for table `message_template`
+--
+ALTER TABLE `message_template`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `content` (`content`),
+  ADD KEY `ix_message_template_id` (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ix_room_room_name` (`room_name`),
+  ADD KEY `ix_room_id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `camera`
+--
+ALTER TABLE `camera`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `line_token`
+--
+ALTER TABLE `line_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `message_template`
+--
+ALTER TABLE `message_template`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `camera`
+--
+ALTER TABLE `camera`
+  ADD CONSTRAINT `camera_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `camera_ibfk_2` FOREIGN KEY (`message_id`) REFERENCES `message_template` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `camera_ibfk_3` FOREIGN KEY (`line_token_id`) REFERENCES `line_token` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
